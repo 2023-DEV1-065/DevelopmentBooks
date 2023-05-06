bbf69c7bdf04fa17ba00a86267188db7cd7177fa
DESKTOP-5CEOGS3\Laptop <issadjeltane@gmail.com>
2023-05-06 01:06:36 -0400

    Adding new unit tests for controller

--- development-books/src/main/java/com/kata/bnpparibasfortis/developmentbooks/service/BookService.java
+++ development-books/src/main/java/com/kata/bnpparibasfortis/developmentbooks/service/BookService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+	private static final double SINGLE_BOOK_PRICE = 50.0;
+	private static final double ONE_HUNDRED = 100;
 	public List<Books> getAllBooks() {
 		return Arrays.stream(BooksEnumeration.values()).map(bookEnum -> new Books(bookEnum.getId(), bookEnum.getTitle(),
 				bookEnum.getAuthor(), bookEnum.getYear(), bookEnum.getPrice())).collect(Collectors.toList());
@@ -26,8 +28,8 @@ public class BookService {
 			return createPriceSummaryWithDiscount(books);
 		}
 	}
-	public PriceSummary createPriceSummaryWithoutDiscount(BookOrder booksInput) {
-		double totalPrice = 50.0 * booksInput.getQuantity();
+	private PriceSummary createPriceSummaryWithoutDiscount(BookOrder booksInput) {
+		double totalPrice = SINGLE_BOOK_PRICE * booksInput.getQuantity();
 		PriceSummary priceSummary = new PriceSummary();
 		priceSummary.setActualPrice(totalPrice);
 		priceSummary.setFinalPrice(totalPrice);
@@ -36,7 +38,7 @@ public class BookService {
 		return priceSummary;
 	}
 
-	public PriceSummary createPriceSummaryWithDiscount(List<BookOrder> books) {
+	private PriceSummary createPriceSummaryWithDiscount(List<BookOrder> books) {
 		int totalBooks = books.stream().mapToInt(BookOrder::getQuantity).sum();
 		List<Integer> bookGroups = new ArrayList<>();
 		double priceOfSimilarBooksLeft = 0;
@@ -59,15 +61,15 @@ public class BookService {
 				+ bookGroups.stream().mapToDouble(this::calculateDiscountAccordingToNumOfBooksInGroup).sum();
 		return createPriceSummary(totalBooks, finalPrice);
 	}
-	public void reduceQuantityOfAddedBooksIntoGroups(List<BookOrder> books) {
+	private void reduceQuantityOfAddedBooksIntoGroups(List<BookOrder> books) {
 		books.forEach(book -> book.setQuantity(book.getQuantity() - 1));
 	}
-	public double calculatePriceWithoutDiscount(List<BookOrder> books) {
+	private double calculatePriceWithoutDiscount(List<BookOrder> books) {
 		return books.stream().filter(book -> book.getQuantity() > 0)
-				.mapToDouble(book -> book.getQuantity() * 50.0).sum();
+				.mapToDouble(book -> book.getQuantity() * SINGLE_BOOK_PRICE).sum();
 	}
 
-	public void optimizeGroups(List<Integer> groups) {
+	private void optimizeGroups(List<Integer> groups) {
 		for (int i = 0; i < groups.size(); i++) {
 			Integer group = groups.get(i);
 			if (group == 5 && groups.contains(3)) {
@@ -76,24 +78,24 @@ public class BookService {
 			}
 		}
 	}
-	public double calculateDiscountAccordingToNumOfBooksInGroup(int groupSize) {
+	private double calculateDiscountAccordingToNumOfBooksInGroup(int groupSize) {
 		double discountedPrice = 0;
-		double actualCost = groupSize * 50.0;
+		double actualCost = groupSize * SINGLE_BOOK_PRICE;
 		if (groupSize == 1)
-			return 50.0;
+			return SINGLE_BOOK_PRICE;
 		if (groupSize == 2)
-			return actualCost * (1 - (5.0 / 100.0));
+			return actualCost * (1 - (5.0 / ONE_HUNDRED));
 		if (groupSize == 3)
-			return actualCost * (1 - (10.0 / 100.0));
+			return actualCost * (1 - (10.0 / ONE_HUNDRED));
 		if (groupSize == 4)
-			return actualCost * (1 - (20.0 / 100.0));
+			return actualCost * (1 - (20.0 / ONE_HUNDRED));
 		if (groupSize == 5)
-			return actualCost * (1 - (25.0 / 100.0));
+			return actualCost * (1 - (25.0 / ONE_HUNDRED));
 		return discountedPrice;
 	}
-	public PriceSummary createPriceSummary(int totalBooks, double finalPrice) {
+	private PriceSummary createPriceSummary(int totalBooks, double finalPrice) {
 		PriceSummary priceSummary = new PriceSummary();
-		priceSummary.setActualPrice(50.0 * totalBooks);
+		priceSummary.setActualPrice(SINGLE_BOOK_PRICE * totalBooks);
 		priceSummary.setFinalPrice(finalPrice);
 		priceSummary.setTotalBooks(totalBooks);
 		priceSummary.setTotalDiscount(priceSummary.getActualPrice() - priceSummary.getFinalPrice());
